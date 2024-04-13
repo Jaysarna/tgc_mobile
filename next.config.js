5afba9266a8339735f83265ce173767019685861
Alok Sahu <tusharsahu2427@gmail.com>
2024-04-13 19:47:14 +0530

    bug fixes on login images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["app.tgc67.online", "tgc67.online"],
+  },
 }
 
 module.exports = nextConfig
